676d649476451d39c1a95f7001d2e2e425c948ae
Rafael Brandão <rafaelbrandao1992@gmail.com>
2022-03-23 18:05:23 -0300

    update

---
+++ api.js
@@ -0,0 +1,30 @@
+async function  searchMovie() {
+
+    searchQuery = document.querySelector('.search-box').value
+    console.log("procurando por " + searchQuery)
+    const request = await fetch("https://imdb-api.com/en/API/Search/k_gzih4xwo/"+searchQuery, {
+        "method": "GET",
+        "headers": {
+            'Accept': 'application/json'
+        }
+    });
+    movies = await request.json();
+    console.log(movies)
+  
+    for (let i = 0; i<movies['results'].length; i++){
+        console.log(movies['results'][i]['image'])
+        console.log("forrrrrrrrrrrrr")
+        temp = `<div class="item">
+            <img src="`+movies['results'][i]['image'] +`"  class="box-filme" alt="" srcset="">
+        </div>`
+        document.querySelector('.search-movies').innerHTML += temp
+    }
+}
+
+const inputEle = document.getElementById('search-box');
+inputEle.addEventListener('keyup', function(e){
+  var key = e.which || e.keyCode;
+  if (key == 13) { // codigo da tecla enter
+    searchMovie();
+  }
+});
