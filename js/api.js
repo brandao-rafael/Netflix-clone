bc64a84202b74e646a037513fd3ce461ab05e66f
themonio92 <rafaelbrandao1992@gmail.com>
2022-03-29 11:25:56 -0300

    modal

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 async function  searchMovie() {
-
+    document.querySelector('.search-movies').innerHTML = ""
     searchQuery = document.querySelector('.search-box').value
     console.log("procurando por " + searchQuery)
     const request = await fetch("https://imdb-api.com/en/API/Search/k_gzih4xwo/"+searchQuery, {
@@ -10,7 +10,7 @@ async function  searchMovie() {
     });
     movies = await request.json();
     console.log(movies)
-  
+    
     for (let i = 0; i<movies['results'].length; i++){
         console.log(movies['results'][i]['image'])
         console.log("forrrrrrrrrrrrr")
@@ -20,3 +20,11 @@ async function  searchMovie() {
         document.querySelector('.search-movies').innerHTML += temp
     }
 }
+
+const inputEle = document.getElementById('search-box');
+inputEle.addEventListener('keyup', function(e){
+  var key = e.which || e.keyCode;
+  if (key == 13) { // codigo da tecla enter
+    searchMovie();
+  }
+});
